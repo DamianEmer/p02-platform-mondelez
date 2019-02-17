d4a26ed83edf98aab413b1397f093216a4860408
Damian <emer_smart17@outlook.com>
2019-02-16 19:20:32 -0600

    Exportacion de reportes a excel
    Se exportan las 3 tablas de reportes a formato excel

--- platform/src/app/shared/components/special-table/special-table.component.ts
+++ platform/src/app/shared/components/special-table/special-table.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, ElementRef } from '@angular/core';
 import { Report } from '../../models/report';
+
+import * as exportAsXLSX from 'xlsx';
 
 @Component({
   selector: 'app-special-table',
@@ -28,6 +30,17 @@ export class SpecialTableComponent implements OnInit {
         divder++;
     })
     return (avg/divder);
+  }
+
+  @ViewChild('table') table: ElementRef;
+
+  exportAsXLSX():void {
+    console.log(this.table);
+    const ws: exportAsXLSX.WorkSheet = exportAsXLSX.utils.table_to_sheet(this.table.nativeElement);
+    const wb: exportAsXLSX.WorkBook = exportAsXLSX.utils.book_new();
+    exportAsXLSX.utils.book_append_sheet(wb, ws, 'Sheet 1');
+
+    exportAsXLSX.writeFile(wb, "sheet1.xlsx");
   }
 
 }
